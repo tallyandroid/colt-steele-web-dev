e5b1a1c3e5e346a9464e0e59dfd4b8ceb4d101d0
Christian Tisby <cduane82@hotmail.com>
2020-03-30 22:30:49 -0400

    adding color toggle button

---
+++ 16_advanced-dom-manipulation/03_color-changer-button/app.js
@@ -0,0 +1,17 @@
+var button = document.querySelector("button")
+var isPurple = false
+
+// button.addEventListener("click", function(){
+//     if(isPurple) {
+//         document.body.style.background = "white" 
+//         isPurple = false
+//     } else {
+//         document.body.style.background = "purple" 
+//         isPurple = true
+//     }
+// })
+
+button.addEventListener("click", function(){
+        document.body.classList.toggle("purple")
+     
+})
