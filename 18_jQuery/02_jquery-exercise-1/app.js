c2e4a62566f281e674750f59fabce533626db2b5
Christian Tisby <cduane82@hotmail.com>
2020-04-07 01:26:51 -0400

    adding jquery

---
+++ 18_jQuery/02_jquery-exercise-1/app.js
@@ -0,0 +1,11 @@
+//Select all divs and give them a purple background
+$("div").css("background", "purple")
+
+//Select the div with the class "highlight" and make them 200px wide
+$("highlight").css("width", "200px")
+
+//Select the div with the id "third" and give it an orange border
+$("#third").css("border", "2px solid orange")
+
+//Select the first div only and change its font color to pink
+$("div:first-of-type").css("color", "pink")
